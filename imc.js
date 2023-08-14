c4c1bf3596d4f3585d8ab4cf773ee1a1d50873a6
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2023-08-14 08:41:21 -0300

    first commit

---
+++ imc.js
@@ -0,0 +1,15 @@
+function calcular(){
+    //Recuperar valor do peso digitado pelo usuário
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let imc = peso / (altura**2);
+    // Concatenando String
+    // alert("O IMC calculado foi: "+ imc);
+    // Template string
+    if(peso<=0 || altura<=0){
+        alert("Peso e altura devem ser maiores que 0!")
+    }
+    else{
+        alert(`O seu IMC é de ${imc.toFixed(2)}`)
+    }
+}
