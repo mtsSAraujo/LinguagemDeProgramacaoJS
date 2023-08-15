7ce489459d1f8e52e88b1ea9cf56f4b5010e42e3
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2023-08-15 10:54:49 -0300

    feat: add ex1 and ex2, with CSS and HTML

---
+++ Lista-1/lista1.js
@@ -0,0 +1,50 @@
+function ex1(){
+
+    let n1 = document.getElementById("ex1N1").value;
+    let n2 = document.getElementById("ex1N2").value;
+    let sub = n1 - n2;
+    document.getElementById("resposta").innerHTML = `O resultado é: ${sub.toFixed(2)}`
+
+}
+
+function ex2(){
+
+    let n1 = document.getElementById("ex2N1").value;
+    let n2 = document.getElementById("ex2N2").value;
+    let n3 = document.getElementById("ex2N3").value;
+    let multiplica = n1 *n2 *n3;
+    document.getElementById("resposta").innerHTML = `O resultado é: ${multiplica.toFixed(2)}`
+}
+
+function ex3(){
+
+}
+
+function ex4(){
+
+}
+
+function ex5(){
+
+}
+
+function ex6(){
+
+}
+
+function ex7(){
+
+}
+
+function ex8(){
+
+}
+
+function ex9(){
+
+}
+
+function ex10(){
+
+}
+
