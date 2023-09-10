4bf9b4a344ab2e90503152c9621d7f0f9d9e6d01
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2023-09-10 12:57:38 -0300

    exercicios 3 e 1 da lista 3

--- Lista-3/lista3.js
+++ Lista-3/lista3.js
@@ -1,6 +1,7 @@
 function ex1(){
 
     let contador2 = 1;
+    let resposta = "";
     while(contador2 <= 5){
         let a = Number(prompt(`Informe A`));
         let b = Number(prompt(`Informe B`));
@@ -29,10 +30,11 @@ function ex1(){
             contador ++;
 
         }
-        console.log(`Ordem crescente: ${a}, ${b}, ${c}, ${d}`);
-        console.log(`Ordem decrescente: ${d}, ${c}, ${b}, ${a}`);
+        resposta += `Ordem crescente: ${a}, ${b}, ${c}, ${d} <br>
+        Ordem decrescente: ${d}, ${c}, ${b}, ${a} <br>`
         contador2 ++;
     }
+    document.getElementById("resposta").innerHTML = `Valores: ${resposta}`;
 
 }
 
@@ -67,6 +69,55 @@ function ex2(){
     corpo = corpo + `<tr><td>${melhorPreco}</td> <td>${melhorQuantidade} </td> <td> ${despesas}</td> <td> ${maiorLucro} </td> </tr>`
     document.getElementById("corpo").innerHTML = corpo;
 }
+
+function ex3(){
+    let resposta = "";
+    let faixa1 = 0;
+    let faixa2 = 0;
+    let faixa3 = 0;
+    let faixa4 = 0;
+    let faixa5 = 0;
+    let flag = 1;
+    let porcentagem1 = 0;
+    let porcentagem2 = 0;
+
+    for(let i = 1; i<=8; i++){
+        let number = Number(prompt(`Informe a ${i}° idade: `));
+
+        if(number > 0 && number <= 15){
+            faixa1 ++;
+        }
+        else if(number > 15 && number <=30){
+            faixa2 ++;
+        }
+        else if(number > 30 && number <= 45){
+            faixa3 ++;
+        }
+        else if(number >35 && number <= 60){
+            faixa4 ++;
+        }
+        else if(number>60){
+            faixa5 ++;
+        }
+        else{
+            resposta = `Valor informado invalido!`;
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1){
+        resposta = `Pessoas por faixa etária: <br> Faixa 1: ${faixa1} <br>
+        Faixa 2: ${faixa2} <br> Faixa 3: ${faixa3} <br>
+        Faixa 4: ${faixa4} <br> Faixa 5: ${faixa5} <br>`
+        porcentagem1 = faixa1/8 * 100;
+        porcentagem2 = faixa5/8 * 100;
+        resposta += `Porcentagem de pessoas na primeira faixa etária: ${porcentagem1}% <br>
+        Porcentagem de pessoas na última faixa etária: ${porcentagem2}%`
+    }
+    document.getElementById("resposta").innerHTML = resposta;
+}
+
 
 function ex4(){
     let number = Number(document.getElementById("ex4n1").value);
