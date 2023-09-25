95e6222ef417ba80e5440d9696e2f1e3133e768f
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2023-09-25 09:01:11 -0300

    aula sobre objetos

---
+++ Lista-5/Exemplo/exemplo.js
@@ -0,0 +1,13 @@
+function exemplo(){
+    // let nome = promt("Informe o nome")
+    // let estoque = prompt("Informe o estoque")
+    // let classificacao = prompt("Informe a classificação")
+    // objeto
+    let objeto = {
+        nome: prompt("Informe o nome"),
+        estoque: Number(prompt("Informe o estoque")),
+        classificacao: prompt("Informe a classificação")
+    }
+
+    console.log(objeto)
+}
