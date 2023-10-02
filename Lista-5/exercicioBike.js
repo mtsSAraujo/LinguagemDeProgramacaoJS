26fa938f29499f8f67a714b6f922f2ae18d342ee
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2023-10-02 08:41:55 -0300

    correção da logica

--- Lista-5/exercicioBike.js
+++ Lista-5/exercicioBike.js
@@ -1,6 +1,6 @@
 function bike(){
     let vetorBicicletas = []
-    for(let i = 0; i<10; i++){
+    for(let i = 0; i<3; i++){
         let bike = {
             marca: prompt("Digite a marca da bicicleta: ").toLowerCase(),
             modelo: prompt("Digite o modelo da bicicleta: ").toLowerCase(),
@@ -20,9 +20,9 @@ function bike(){
     let vetorCaloi = []
     let maiorQuadro = 0;
     let posicaoMaiorQuadro = 0;
-    for(i=0;i<10;i++){
+    for(i=0;i<3;i++){
         soma += vetorBicicletas[i].preco
-        if(antiga < vetorBicicletas[i].ano){
+        if(antiga > vetorBicicletas[i].ano){
             antiga = vetorBicicletas[i].ano
         }
         if(vetorBicicletas[i].marca === "caloi"){
@@ -32,23 +32,26 @@ function bike(){
             aro29 ++
         }
         if(vetorBicicletas[i].quadro > maiorQuadro){
-            maiorQuadro = quadro
+            maiorQuadro = vetorBicicletas[i].quadro
             posicaoMaiorQuadro = i
         }
     }
 
-    for(i=0;i<10;i++){
+    for(i=0;i<3;i++){
         if(vetorBicicletas[i].ano == antiga){
             vetorAntigas.push(vetorBicicletas[i])
+            console.log("bike antiga inserida")
         }
     }
     if(soma != 0){
-        media = soma/10
+        media = soma/3
     }
 
     console.log("A média de preço das bikes é: \nR$" + media)
-    console.log("As bikes mais antigas são: \n"+ vetorAntigas)
+    console.log("As bikes mais antigas são: ")
+    console.log(vetorAntigas)
     console.log("As bikes da marca caloi sao as de posição: \n" + vetorCaloi)
     console.log(aro29 + " bikes possuem aro 29")
-    console.log("A bike " + vetorBicicletas[posicaoMaiorQuadro] + " possui o maior quadro")
+    console.log("A bike que possui o maior quadro é: ")
+    console.log(vetorBicicletas[posicaoMaiorQuadro])
 }
